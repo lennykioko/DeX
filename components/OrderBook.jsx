69bb93bfb239ba3be56e6b7d23db7ed2e8c5aa6e
lenny <lennykmutua@gmail.com>
2022-08-30 22:07:11 +0300

    Complete Static UI setup (#5)

---
+++ components/OrderBook.jsx
@@ -0,0 +1,132 @@
+function OrderBook() {
+  return (
+    <div className="row-span-6 border-2 border-black">
+      <div className="border-b-2 border-gray-900 p-4 text-xl">OrderBook</div>
+      <div className="w-full">
+        <table className="w-full table-auto border-collapse text-center">
+          <thead>
+            <tr>
+              <th scope="col" className="border border-slate-700 py-2">
+                DAPP
+              </th>
+              <th scope="col" className="border border-slate-700 py-2">
+                DAPP/ETH
+              </th>
+              <th scope="col" className="border border-slate-700 py-2">
+                ETH
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th
+                scope="row"
+                className="border border-slate-700 py-1 font-normal"
+              >
+                10
+              </th>
+              <td className="border border-slate-700 py-1">0.001</td>
+              <td className="border border-slate-700 py-1">0.01</td>
+            </tr>
+
+            <tr>
+              <th
+                scope="row"
+                className="border border-slate-700 py-1 font-normal"
+              >
+                10
+              </th>
+              <td className="border border-slate-700 py-1">0.001</td>
+              <td className="border border-slate-700 py-1">0.01</td>
+            </tr>
+
+            <tr>
+              <th
+                scope="row"
+                className="border border-slate-700 py-1 font-normal"
+              >
+                10
+              </th>
+              <td className="border border-slate-700 py-1">0.001</td>
+              <td className="border border-slate-700 py-1">0.01</td>
+            </tr>
+
+            <tr>
+              <th
+                scope="row"
+                className="border border-slate-700 py-1 font-normal"
+              >
+                10
+              </th>
+              <td className="border border-slate-700 py-1">0.001</td>
+              <td className="border border-slate-700 py-1">0.01</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <table className="mt-4 w-full table-auto border-collapse text-center">
+          <thead>
+            <tr>
+              <th scope="col" className="border border-slate-700 py-2">
+                DAPP
+              </th>
+              <th scope="col" className="border border-slate-700 py-2">
+                DAPP/ETH
+              </th>
+              <th scope="col" className="border border-slate-700 py-2">
+                ETH
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th
+                scope="row"
+                className="border border-slate-700 py-1 font-normal"
+              >
+                10
+              </th>
+              <td className="border border-slate-700 py-1">0.001</td>
+              <td className="border border-slate-700 py-1">0.01</td>
+            </tr>
+
+            <tr>
+              <th
+                scope="row"
+                className="border border-slate-700 py-1 font-normal"
+              >
+                10
+              </th>
+              <td className="border border-slate-700 py-1">0.001</td>
+              <td className="border border-slate-700 py-1">0.01</td>
+            </tr>
+
+            <tr>
+              <th
+                scope="row"
+                className="border border-slate-700 py-1 font-normal"
+              >
+                10
+              </th>
+              <td className="border border-slate-700 py-1">0.001</td>
+              <td className="border border-slate-700 py-1">0.01</td>
+            </tr>
+
+            <tr>
+              <th
+                scope="row"
+                className="border border-slate-700 py-1 font-normal"
+              >
+                10
+              </th>
+              <td className="border border-slate-700 py-1">0.001</td>
+              <td className="border border-slate-700 py-1">0.01</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
+}
+
+export default OrderBook
